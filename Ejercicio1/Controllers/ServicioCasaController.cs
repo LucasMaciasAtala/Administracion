60e89a03b6c5dd2b6eab57f7831f651944feccf6
Lucas Macias <lucas12macias@gmail.com>
2019-04-05 18:27:25 -0300

    Added Method and View

---
+++ Ejercicio1/Controllers/ServicioCasaController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Ejercicio1.Models;
+using System.Web.Mvc;
+
+
+
+namespace Ejercicio1.Controllers
+{
+    public class ServicioCasaController : Controller
+    {
+        // GET: ServicioCasa
+        public ActionResult ObtenerCasa()
+        {
+            var casa = new Casa()
+            {
+                Id = 0,
+                Propietario = new Cocinero() { Id = 0, Nombre = "Carlos", Apellido = "Mesina", Documento = 33432543, Pedidos = new List<int>() },
+                Descripcion = "Casa fantasma",
+                Direccion = "Acá nomás"
+            };
+            
+            return View(casa);
+        }
+    }
+}
